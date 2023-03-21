c5834e6919d9405e7443989b1b6c760938c59977
Joshua <frozen.hell08@gmail.com>
2023-03-21 14:51:42 +0800

    StateControl
    login and logout

--- app/Http/Controllers/Api/StateControl.php
+++ app/Http/Controllers/Api/StateControl.php
@@ -3,9 +3,48 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class StateControl extends Controller
 {
-    //
+    public function login(Request $request) {
+        $validation = Validator::make($request->all(), [
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if ($validation->fails()) {
+            return response()->json([
+                'errors' => $validation->errors(),
+            ], Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        $credentials = $request->only('email', 'password');
+
+        if (! $token = auth()->attempt($credentials)) {
+            return response()->json([
+
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        return response()->json([
+            'message' => 'User logged in',
+            'account' => auth()->user(),
+            'token' => $token,
+            'expires_in' => Auth::factory()->getTTL() * 60,
+            'payload' => Auth::payload(),
+        ], Response::HTTP_CREATED);
+    }
+
+    public function logout() {
+        auth()->logout();
+
+        return response()->json([
+            'message' => 'User has been logged out.'
+        ], Response::HTTP_OK);
+    }
 }
