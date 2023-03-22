3e99365107ef962e368c880d15a72f7d099bca7a
Joshua <frozen.hell08@gmail.com>
2023-03-22 13:39:05 +0800

    Time Table update
    time_in constraint

--- database/migrations/2023_03_20_101816_create_time_tables_table.php
+++ database/migrations/2023_03_20_101816_create_time_tables_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -16,9 +17,11 @@ return new class extends Migration
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->date('date');
             $table->timestamp('time_in');
-            $table->timestamp('time-out')->nullable();
+            $table->timestamp('time_out')->nullable();
             $table->timestamps();
         });
+
+        DB::statement('ALTER TABLE time_tables MODIFY COLUMN time_in TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP');
     }
 
     /**
